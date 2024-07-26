1b0e04934db7b6d37ef4a9c57dff12f825cb9c60
BuhleMtshali <mtshalizanobuhle7@gmail.com>
2024-07-26 18:41:53 +0200

    updated the index.js

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 let inputCheckBox = document.getElementById("dark-mode-checkbox");
 let headings = document.querySelectorAll("h1");
 let cardsBg = document.querySelectorAll(".cards");
-let headingTwo = document.querySelector("h2");
+let headingTwo = document.querySelectorAll("h2");
 let lighterText = document.querySelectorAll(".lighter-text");
 let innerHeadings = document.querySelectorAll(".heading");
+let bodyBg = document.querySelector("body");
+let handle = document.getElementsByClassName("handle");
+//begining of function
+
+inputCheckBox.addEventListener("change", lightMode);
+function lightMode() {
+  bodyBg.style.backgroundColor = "hsl(0, 0%, 100%)";
+  headingTwo.forEach((heading) => {
+    heading.style.color = "hsl(230, 17%, 14%)";
+  });
+  headings.forEach((h1) => {
+    h1.style.color = "hsl(230, 17%, 14%)";
+  });
+  cardsBg.forEach((cards) => {
+    cards.style.backgroundColor = "hsl(227, 47%, 96%)";
+  });
+}
