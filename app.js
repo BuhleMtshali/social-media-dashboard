428748e67325457946b2562653ec883a8ef96d0b
BuhleMtshali <mtshalizanobuhle7@gmail.com>
2024-07-26 18:11:36 +0200

    updated the javascript file

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-alert("hello");
+let inputCheckBox = document.getElementById("dark-mode-checkbox");
+let headings = document.querySelectorAll("h1");
+let cardsBg = document.querySelectorAll(".cards");
+let headingTwo = document.querySelector("h2");
+let lighterText = document.querySelectorAll(".lighter-text");
+let innerHeadings = document.querySelectorAll(".heading");
